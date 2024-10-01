5e44be244c7b7126b2736ec931ff2fa19495389a
Mike HP <tutoruialsspartan@gmail.com>
2024-09-30 18:33:40 -0600

    Rutas & Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path:'ejercicio1',
+    loadComponent: () => import('./components/ejercicio1/ejercicio1.component').then(m => m.Ejercicio1Component)
+  },
+  {
+    path:'ejercicio2',
+    loadComponent: () => import('./components/ejercicio2/ejercicio2.component').then(m => m.Ejercicio2Component)
+  },
+  {
+    path:'ejercicio3',
+    loadComponent: () => import('./components/ejercicio3/ejercicio3.component').then(m => m.Ejercicio3Component)
+  },
+  {
+    path:'ejercicio4',
+    loadComponent: () => import('./components/ejercicio4/ejercicio4.component').then(m => m.Ejercicio4Component)
+  },
+  {
+    path:'ejercicio5',
+    loadComponent: () => import('./components/ejercicio5/ejercicio5.component').then(m => m.Ejercicio5Component)
+  },
+  {
+    path:'ejercicio6',
+    loadComponent: () => import('./components/ejercicio6/ejercicio6.component').then(m => m.Ejercicio6Component)
+  },
+  {
+    path:'ejercicio7',
+    loadComponent: () => import('./components/ejercicio7/ejercicio7.component').then(m => m.Ejercicio7Component)
+  }
+];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  exercises = [
+    { title: 'Ejercicio 1', description: 'Contar ceros en una matriz 4x4', link: '/ejercicio1' },
+    { title: 'Ejercicio 2', description: 'Cuadrado Mágico', link: '/ejercicio2' },
+    { title: 'Ejercicio 3', description: 'Operaciones con matrices 2x2', link: '/ejercicio3' },
+    { title: 'Ejercicio 4', description: 'Llenar matriz con 1\'s en la diagonal', link: '/ejercicio4' },
+    { title: 'Ejercicio 5', description: 'Suma y promedio de filas y columnas', link: '/ejercicio5' },
+    { title: 'Ejercicio 6', description: 'Resumen de ventas por mes y día', link: '/ejercicio6' },
+    { title: 'Ejercicio 7', description: 'Resumen de calificaciones', link: '/ejercicio7' }
+  ];
 }
